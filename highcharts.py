f425e891a83f3146a1928b257179ae6deb1d97ca
Ryan A Whitley <ryan45@uab.edu>
2019-06-06 17:10:49 -0500

    Documentation/Cleanup

--- highcharts.py
+++ highcharts.py
@@ -1,8 +1,27 @@
 __author__ = 'Ryan Whitley'
 
 class Highchart:
+    """ Class for converting data to be plotted into boilerplate highcharts javascript code """
 
     def __init__(self, x: 'List', y: 'List', chart_options: 'List'):
+        """ Init method for the class
+        
+            Args:
+                x: 2D Array Containing x-coordinate information
+                y: 2D Array Containing y-coordinate information
+                chart_options: Array containing chart information as follows:
+                    chart_options[0](str): Chart Title (optional blank)
+                    chart_options[1](str): Type of Plot
+                    chart_options[2](str): X-Axis Type(category, datetime, etc..)
+                    chart_options[3](List[str]): Categories Array (optional blank)
+                    chart_options[4](str): Y-Axis Title (optional blank)
+                    chart_options[5](List[str]): Array representing colors of different series (optional blank)
+                    chart_options[6](List[str]): Array of series titles
+                    chart_options[7](str): div name (for embedding)
+                    chart_options[8](List[ List[high,low] ]): Standard Deviations (optional blank)
+                    chart_options[9](str): Perform linear or polnomial regression (optional blank)
+        
+        """
         if (len(x) != len(y) or
                 (len(chart_options[5]) > 0 and len(chart_options[5]) != len(x)) or
                 (len(chart_options[6]) > 0 and len(chart_options[6]) != len(x)) or
@@ -25,52 +44,56 @@ class Highchart:
         self.cats = chart_options[3]
         self.y_title = chart_options[4]
         self.colors = chart_options[5]
-        self.lgnd_titles = chart_options[6]
+        self.name = chart_options[6]
         self.div = chart_options[7]
         self.error = chart_options[8]
         self.regression = chart_options[9]
 
-    def format_series(self, x, y, series_type, colors, name, error, regression) -> 'str':
-        """ Returns (multiple) series as Highcharts Formatted String """
+    def format_series(self) -> 'str':
+        """ Returns (multiple) series as Highcharts Formatted String 
+
+            Note:
+                Maintaining consistency accross the data is important here
+        """
         series = ""
-        if error:
-            for i in range(len(error)):
+        if self.error:
+            for i in range(len(self.error)):
                 series += "{\n"
-                series += "name: '" + name[i] + " error',\n"
+                series += "name: '" + self.name[i] + " error',\n"
                 series += "type: 'errorbar',\n"
                 series += "data: ["
-                for j in range(len(error[i])):
-                    if j != len(error[i]) - 1:
-                        series += "[" + str(error[i][j][0]) + \
-                            "," + str(error[i][j][1]) + "],"
+                for j in range(len(self.error[i])):
+                    if j != len(self.error[i]) - 1:
+                        series += "[" + str(self.error[i][j][0]) + \
+                            "," + str(self.error[i][j][1]) + "],"
                     else:
-                        series += "[" + str(error[i][j][0]) + \
-                            "," + str(error[i][j][1]) + "]]"
+                        series += "[" + str(self.error[i][j][0]) + \
+                            "," + str(self.error[i][j][1]) + "]]"
 
                 series += "\n},"
 
-        for i in range(len(x)):
+        for i in range(len(self.x)):
             series += "{\n"
-            series += "type: '" + series_type + "',\n"
+            series += "type: '" + self.plot + "',\n"
 
-            if regression:
+            if self.regression:
                 series += "regression: true,\n"
                 series += "regressionSettings: {\n"
                 series += 'name: "r2: %r",\n'
                 series += "color: 'red',\n"
-                series += "type: '" + regression + "'\n},\n"
+                series += "type: '" + self.regression + "'\n},\n"
 
-            if colors:
-                series += "color: '" + colors[i] + "',\n"
-            if name:
-                series += "name: '" + name[i] + "',\n"
+            if self.colors:
+                series += "color: '" + self.colors[i] + "',\n"
+            if self.name:
+                series += "name: '" + self.name[i] + "',\n"
             series += "data: ["
-            for j in range(len(x[i])):
-                if j != len(x[i]) - 1:
-                    series += "[" + str(x[i][j]) + "," + str(y[i][j]) + "],"
+            for j in range(len(self.x[i])):
+                if j != len(self.x[i]) - 1:
+                    series += "[" + str(self.x[i][j]) + "," + str(self.y[i][j]) + "],"
                 else:
-                    series += "[" + str(x[i][j]) + "," + str(y[i][j]) + "]]"
-            if i != len(x) - 1:
+                    series += "[" + str(self.x[i][j]) + "," + str(self.y[i][j]) + "]]"
+            if i != len(self.x) - 1:
                 series += "\n},"
             else:
                 series += "\n}"
@@ -105,7 +128,7 @@ class Highchart:
         """
 
     def get_imports(self):
-        """ Imports for Jquery and Highcarts """
+        """ Imports for Jquery and Highcarts+plugins """
         return """
     <script
       src="https://code.jquery.com/jquery-3.3.1.min.js"
@@ -120,9 +143,9 @@ class Highchart:
         """
 
     def highcharts_str(self) -> 'str':
+        """ returns full JavaScript coded string for embedding """
         boiler = self.js_boiler()
-        hc_str = self.format_series(self.x, self.y, self.plot, self.colors,
-                               self.lgnd_titles, self.error, self.regression)
+        hc_str = self.format_series()
         js_full = boiler % (self.div, self.title, self.x_type, self.cats, self.y_title,
                             hc_str)
         return js_full
